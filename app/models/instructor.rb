e99695a41b712e392b3810ad30a1a7e6fb3404cb
Jp1200 <jprkr96@gmail.com>
2020-02-02 15:55:53 -0600

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
 class Instructor
+attr_reader :instructor_name
+@@all =[]
+  def initialize(instructor_name)
+    @instructor_name = instructor_name
+    @@all << self
 
+  end
+
+def pass_student(student, test_name)
+  BoatingTest.all.each do |i|
+    if i.student == student && i.test_name == test_name
+      i.test_status = 'passed'
+    end
+  end
+  BoatingTest.new(student, test_name, 'passed', self)
+
+end
+
+def fail_student(student, test_name)
+  BoatingTest.all.each do |i|
+    if i.student == student && i.test_name == test_name
+      i.test_status = 'failed'
+    end
+  end
+  BoatingTest.new(student, test_name, 'failed', self)
+
+
+
+end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,33 @@
 class Student
 
+  @@all = []
+  attr_reader :first_name
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+      #add_boating_test should initialize a new boating test with a Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+      BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def self.find_student(first_name)
+    self.all.find {|i| i.first_name == first_name}
+
+  end
+
+  def grade_percentage(first_name)
+
+    tests = BoatingTest.all.select {|i| i.student.first_name == first_name}
+
+    p_tests = tests.count{|tes| tes.test_status == 'passed'}
+    #binding.pry
+    return (p_tests)/(tests.count).to_f * 100.0
+  end
 end
